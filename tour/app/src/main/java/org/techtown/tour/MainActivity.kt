355b1876a334ad06d0521c5c6568a3479799dc98
jaehyun <wwogusqkr0515@naver.com>
2021-09-21 00:54:08 +0900

    fix

--- tour/app/src/main/java/org/techtown/tour/MainActivity.kt
+++ tour/app/src/main/java/org/techtown/tour/MainActivity.kt
@@ -2,10 +2,39 @@ package org.techtown.tour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val fl: FrameLayout by lazy {
+        findViewById(R.id.main_frame)
+    }
+    private val bn: BottomNavigationView by lazy{
+        findViewById(R.id.bottomNavigationView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bn.setOnNavigationItemSelectedListener {
+            replaceFragment(
+                when (it.itemId) {
+                    R.id.home -> FragmentOne()
+                    else -> FragmentTwo()
+
+                }
+            )
+            true
+        }
+
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(1).isEnabled = false
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(fl.id, fragment).commit()
     }
 }
